fa995a0ba0aff6279a6fabc3f51953d5674d3b0a
Denis K <orca84@protonmail.com>
2020-12-04 14:12:23 +1000

    ADD: connect server and client

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,11 @@
 use clap::Clap;
 use futures_channel::mpsc::{unbounded, UnboundedSender};
 use futures_util::{
-    // future,
+    future,
     future::{select, Either},
-    // pin_mut,
-    // stream::TryStreamExt,
-    // SinkExt,
-    StreamExt,
+    pin_mut,
+    stream::TryStreamExt,
+    SinkExt, StreamExt, TryFutureExt,
 };
 use std::{
     collections::HashMap,
@@ -14,10 +13,10 @@ use std::{
     sync::{Arc, Mutex},
     time::Duration,
 };
-// use tokio::io::{AsyncReadExt, AsyncWriteExt};
+use tokio::io::{AsyncReadExt, AsyncWriteExt};
 use tokio::net::{TcpListener, TcpStream};
 // use tokio_tungstenite::{accept_async, connect_async, tungstenite::Error};
-use tokio_tungstenite::connect_async;
+use tokio_tungstenite::{connect_async, WebSocketStream};
 use tungstenite::protocol::Message;
 use url::Url;
 
@@ -34,16 +33,17 @@ struct Opts {
     connect: Option<String>,
 }
 
-async fn server(peers: Peers, stream: TcpStream, address: SocketAddr, period: u64) {
+async fn handler(peers: Peers, stream: TcpStream, address: SocketAddr, period: u64) {
     let ws_stream = tokio_tungstenite::accept_async(stream)
         .await
         .expect("Failed to accept");
 
-    // Insert the write part of this peer to the peer map.
+    // TODO: send peers list to client
+
     let (tx, rx) = unbounded();
     peers.lock().unwrap().insert(address, tx);
 
-    let (mut ws_sender, mut ws_receiver) = ws_stream.split();
+    let (ws_sender, mut ws_receiver) = ws_stream.split();
     let mut interval = tokio::time::interval(Duration::from_secs(period));
 
     let mut message_future = ws_receiver.next();
@@ -68,13 +68,10 @@ async fn server(peers: Peers, stream: TcpStream, address: SocketAddr, period: u6
                 };
             }
             Either::Right((_, msg_fut_continue)) => {
+                println!("send");
                 let peers = peers.lock().unwrap();
-                let broadcast_recipients = peers
-                    .iter()
-                    .filter(|(peer_addr, _)| peer_addr != &&address)
-                    .map(|(_, ws_sink)| ws_sink);
-
-                for r in broadcast_recipients {
+                for (a, r) in peers.iter() {
+                    println!("a: {}", a);
                     r.unbounded_send(Message::Text("Hello!".to_owned()))
                         .unwrap();
                 }
@@ -89,38 +86,75 @@ async fn server(peers: Peers, stream: TcpStream, address: SocketAddr, period: u6
     peers.lock().unwrap().remove(&address);
 }
 
-async fn client(peers: Peers, stream: WebSocketStream, address: SocketAddr, period: u64) {
-    //----------------------------------
-}
-
 #[tokio::main]
 async fn main() {
     let opts: Opts = Opts::parse();
 
     let peers = Peers::new(Mutex::new(HashMap::new()));
+    let my_address = format!("127.0.0.1:{}", opts.port);
 
     match opts.connect {
         // as client
         Some(server_address) => {
             let url = Url::parse(format!("ws://{}", server_address).as_ref()).unwrap();
             let (ws_stream, _) = connect_async(url).await.expect("Failed to connect");
-            println!("My address is 127.0.0.1:{}", opts.port);
+            println!("My address is {}", my_address);
 
-            // tokio::spawn(client(
-            //     peers.clone(),
-            //     ws_stream,
-            //     server_address,
-            //     opts.period,
-            // ));
+            let (mut sink, stream) = ws_stream.split();
+            loop {
+                sink.send(tungstenite::Message::Text("yo!".to_string()))
+                    .map_err(|e| ())
+                    .await;
+            }
+
+            // let (tx, rx) = unbounded();
+            // let (stdin_tx, stdin_rx) = futures_channel::mpsc::unbounded::<Message>();
+
+            // let (ws_sender, mut ws_receiver) = ws_stream.split();
+            // let mut interval = tokio::time::interval(Duration::from_secs(opts.period));
+            //
+            // let mut message_future = ws_receiver.next();
+            // let mut tick_future = interval.next();
+            //
+            // loop {
+            //     match select(message_future, tick_future).await {
+            //         Either::Left((message, tick_fut_continue)) => {
+            //             println!("we have a message! {:?}", message);
+            //             match message {
+            //                 Some(message) => {
+            //                     if let Ok(message) = message {
+            //                         if message.is_close() {
+            //                             break;
+            //                         }
+            //                         println!("Received message {} from {}", message, my_address);
+            //                     };
+            //
+            //                     tick_future = tick_fut_continue;
+            //                     message_future = ws_receiver.next();
+            //                 }
+            //                 None => break,
+            //             };
+            //         }
+            //         Either::Right((_, msg_fut_continue)) => {
+            //             println!("time to send a message!");
+            //             // let peers = peers.lock().unwrap();
+            //             // for (_, r) in peers.iter() {
+            //             //     r.unbounded_send(Message::Text("Hi!".to_owned())).unwrap();
+            //             // }
+            //
+            //             message_future = msg_fut_continue;
+            //             tick_future = interval.next();
+            //         }
+            //     }
+            // }
         }
         // as server
         None => {
-            let my_address = format!("127.0.0.1:{}", opts.port);
             let listener = TcpListener::bind(&my_address).await.expect("Can't listen");
             println!("My address is {}", my_address);
 
             while let Ok((stream, address)) = listener.accept().await {
-                tokio::spawn(server(peers.clone(), stream, address, opts.period));
+                tokio::spawn(handler(peers.clone(), stream, address, opts.period));
             }
         }
     }
